43cbec37ae132a8b7c812fbd01b4cf227a7b277f
gyaneman <gyanexus7@gmail.com>
2017-11-07 16:47:34 +0900

    small changes

--- src/interp.ml
+++ src/interp.ml
@@ -14,27 +14,28 @@ and interp_stmts stmtlist env =
   match stmtlist with
   | [] -> ()
   | stmt :: stmtlist_ ->
-      let new_env = match stmt with
-      | Block (stmtlist__) -> interp_stmts stmtlist__ env; env
+      match stmt with
+      | Block (stmtlist__) ->
+          interp_stmts stmtlist__ env;
+          interp_stmts stmtlist_ env;
       | For (init, cond, update, body) ->
           raise Not_implemented_yet
       | If (cond, conseq, alter) ->
           match (eval cond env) with
           | BoolVal (b) ->
-              if b then begin
-                interp_stmts conseq env; env;
-              end else begin
-                interp_stmts alter env; env;
-              end
+              if b then
+                interp_stmts conseq env
+              else
+                interp_stmts alter env
           | _ -> raise Type_error;
           ;
+          interp_stmts stmtlist_ env;
       | Expression (exp) ->
-          print_value (eval exp env); env;
+          print_value (eval exp env);
+          interp_stmts stmtlist_ env;
       | VarDecl (id, exp) ->
           let exp_val = eval exp env in
-          extend_env id exp_val env;
-      in
-      interp_stmts stmtlist_ new_env
+          interp_stmts stmtlist_ (extend_env id exp_val env);
   ;
 and eval exp env =
   match exp with
