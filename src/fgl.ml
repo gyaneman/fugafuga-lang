8a290b0e9027d91b449f5b0fbf10c1da4012367a
gyaneman <gyanexus7@gmail.com>
2017-11-16 13:36:36 +0900

    impl -a option which enable AST printing

--- src/fgl.ml
+++ src/fgl.ml
@@ -8,15 +8,20 @@ open Value
 open Environment
 open Interp
 
+let enable_only_ast_print = ref false
+
 let exec_file fname =
   let ic = open_in fname in
   try
     let lexbuf = Lexing.from_channel ic in
     let ast = Parser.main Lexer.token lexbuf in
     close_in ic;
-    print_string (string_of_program ast);
-    print_newline();
-    let result = interp ast [] in print_value result;
+    if !enable_only_ast_print then
+      print_string (string_of_program ast)
+    else
+      let result = interp ast [] in
+      print_value result;
+    ;
     flush stdout;
   with e ->
     close_in_noerr ic;
@@ -26,7 +31,9 @@ let exec_file fname =
 let usagemsg = "./fgl.byte program.fgl"
 
 let main =
-  let speclist = [] in
+  let speclist = [
+    ("-a", Arg.Set enable_only_ast_print, "pretty printing AST")
+    ] in
   Arg.parse speclist exec_file usagemsg
 ;;
 
